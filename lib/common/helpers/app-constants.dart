b51bf92bd2729f239a32fed0ee3c145fbc72391f
Vladislav <user@users-MacBook-Pro.local>
2021-10-08 14:05:03 +0300

    change base url

--- lib/common/helpers/app-constants.dart
+++ lib/common/helpers/app-constants.dart
@@ -1,7 +1,6 @@
 class AppConstants {
   static const String lastProject = 'lastProject';
-  static const String baseUrl = 'http://localhost:8090';
-  // static const String baseUrl = 'https://jsdaddy.io/company-id/api';
+  static const String baseUrl = 'https://jsdaddy.io/company-id/api';
   static const String slackUrl =
       'https://app.slack.com/client/T5WCD8R5H/user_profile';
   static const String Statistics = 'Statistics';
